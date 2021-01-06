72e2b85606eeda7d5f16cff36275942520b70835
mjtillman <mjtillman@gmail.com>
2021-01-05 22:29:08 -0600

    defeated day 3 part 1 whooo

---
+++ src/TrajectoryTrial.scala
@@ -0,0 +1,55 @@
+import java.io.{BufferedReader, FileReader}
+
+import scala.collection.mutable.ArrayBuffer
+
+object TrajectoryTrial extends App {
+
+//  class trajMap {
+//    val trajMap: Array.ofDim[Char]
+//  }
+
+  val run = 3
+  val rise = 1
+
+  val fName = "src/trajectoryTrialMap.txt"
+  val fReader = new BufferedReader(new FileReader(fName))
+  val arrBuf = new ArrayBuffer[Array[Char]]()
+
+  def fHandler(newLine: String): Array[Array[Char]] = {
+    if (newLine != null) {
+      arrBuf += newLine.toCharArray
+      fHandler(fReader.readLine())
+    }
+    arrBuf.toArray
+  }
+
+  val arr = fHandler(fReader.readLine())
+  val width = arr(0).length
+  val height = arr.length
+
+  fReader.close()
+
+//  println(arr.map(_.mkString(" ")).mkString("\n"))
+
+
+  def pathGen(y: Int = 0, x: Int = 0): Array[Char] = {
+    val pathBuf = new ArrayBuffer[Char]()
+
+    def getStep(y: Int, x: Int): Unit = {
+      if (y < height) {
+        println(s"($x, $y): ${arr(y)(x)}")
+        pathBuf += arr(y)(x)
+
+        getStep(y + rise, (x + run) % width)
+
+      }
+    }
+
+    getStep(y + rise, x + run)
+
+    pathBuf.toArray
+  }
+
+  val path = pathGen()
+  println(s"\nPath: ${path.mkString(" ")}")
+}
